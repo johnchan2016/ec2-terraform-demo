8c14dee409a94500452f6b6092384e0a156f5f58
johnchan <myhk2009@gmail.com>
2021-05-31 22:40:43 +0800

    add private subnet

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -2,6 +2,8 @@ resource "aws_subnet" "public-subnet-1" {
   vpc_id            = var.vpc_id
   cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
+  map_public_ip_on_launch = true
+
   tags = {
     Name = "${var.env_prefix}-subnet-1"
   }
@@ -11,6 +13,7 @@ resource "aws_subnet" "private-subnet-1" {
   vpc_id            = var.vpc_id
   cidr_block        = var.private_subnet_cidr_block
   availability_zone = var.avail_zone
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "${var.env_prefix}-prviate-subnet-1"
@@ -19,6 +22,7 @@ resource "aws_subnet" "private-subnet-1" {
 
 resource "aws_default_route_table" "main-rtb" {
   default_route_table_id = var.default_route_table_id
+  #vpc_id                = var.vpc_id
 
   route {
     cidr_block = "0.0.0.0/0"
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,6 +1,6 @@
 vpc_cidr_block            = "10.0.0.0/16"
 subnet_cidr_block         = "10.0.1.0/24"
-private_subnet_cidr_block = "10.0.10.0/24"
+private_subnet_cidr_block = "10.0.2.0/24"
 avail_zone                = "eu-west-1b"
 env_prefix                = "dev"
 instance_type             = "t2.micro"
